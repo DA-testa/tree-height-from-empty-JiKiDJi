df6888bef7e0fb51a73f66669b26b3913472a1fc
Alekss Verskovs <64939153+JiKiDJi@users.noreply.github.com>
2023-02-20 13:38:22 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -25,7 +25,7 @@ def compute_height(n, parents):
 def main():
     let = input()[0]
     if let == 'F' :
-        text = "./text/" + input() +".txt"
+        text = sys.path[0]+ "text/" + input() +".txt"
         print(text)
         if 'a' in text:
             return
